dd94f0173939ffb966da6d206093dc7f79e18c6a
Nicolas Fraisse <ni.fraisse@gmail.com>
2024-09-04 21:49:34 -0400

    update firebase config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "main.js",
   },
-  devtool: "sourcemap"
+  devtool: "sourcemap",
 };
